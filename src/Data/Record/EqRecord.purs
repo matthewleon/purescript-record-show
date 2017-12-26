cc0b676384ad851130c2dde30abc2c2f81f84fd9
Tomasz Rybarczyk <paluho@gmail.com>
2017-12-26 13:05:59 +0100

    Drop unicode syntax

--- src/Data/Record/EqRecord.purs
+++ src/Data/Record/EqRecord.purs
@@ -13,7 +13,7 @@ import Type.Row (kind RowList, Nil, Cons, RLProxy(..))
 
 -- | Eq for subrecord given by a row list
 class EqRowList (list :: RowList) (row :: # Type) where
-  eqRowList :: RLProxy list -> Record row → Record row -> Boolean
+  eqRowList :: RLProxy list -> Record row -> Record row -> Boolean
 
 instance _a_eqRowListNil :: EqRowList Nil a where
   eqRowList _ _ _ = true
@@ -25,11 +25,11 @@ instance _b_eqRowListConsRecord ::
   , IsSymbol key
   , RowCons key (Record r) t row) => EqRowList (Cons key (Record r) listRest) row where
   eqRowList _ rec1 rec2 =
-    eqRowList (RLProxy ∷ RLProxy rl) s1 s2
-    && eqRowList (RLProxy ∷ RLProxy listRest) rec1 rec2
+    eqRowList (RLProxy :: RLProxy rl) s1 s2
+    && eqRowList (RLProxy :: RLProxy listRest) rec1 rec2
    where
-    s1 = get (SProxy ∷ SProxy key) rec1
-    s2 = get (SProxy ∷ SProxy key) rec2
+    s1 = get (SProxy :: SProxy key) rec1
+    s2 = get (SProxy :: SProxy key) rec2
 
 instance _c_eqRowListCons ::
   ( Eq a
@@ -37,14 +37,16 @@ instance _c_eqRowListCons ::
   , IsSymbol key
   , RowCons key a r row) => EqRowList (Cons key a listRest) row where
   eqRowList _ rec1 rec2 =
-    get (SProxy ∷ SProxy key) rec1 == get (SProxy ∷ SProxy key) rec2
-    && eqRowList (RLProxy ∷ RLProxy listRest) rec1 rec2
+    get (SProxy :: SProxy key) rec1 == get (SProxy :: SProxy key) rec2
+    && eqRowList (RLProxy :: RLProxy listRest) rec1 rec2
 
 eqRecord
   :: forall row list
    . RowToList row list
   => EqRowList list row
   => Record row
-  → Record row
+  -> Record row
   -> Boolean
-eqRecord rec1 rec2 = eqRowList (RLProxy ∷ RLProxy list) rec1 rec2
+eqRecord rec1 rec2 = eqRowList (RLProxy :: RLProxy list) rec1 rec2
+
+
